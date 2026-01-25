aeb9e18537786f6c0d123e54b85bb95cb1b13876
anilsolanki9 <anilsolanki1192@gmail.com>
2026-01-25 14:44:41 +0530

    practice day-1

--- day-1/index.js
+++ day-1/index.js
@@ -1,9 +1,37 @@
-const express = require('express');
+// running js code in terminal
+// console.log("Hello world");
+// shortcut to open terminal - > ctr + `
+// open terminal and write $node index.js
+// Output => Hello world
+// for (let i = 0; i < 100; i++) console.log(i);
 
-const app = express(); // server ka instance create hua and stored in app
+//
+// Using packages
+// install package from https://www.npmjs.com
+// npm i <package-name>
+// to use package require it, and store in a constant
+// const catMe = require("cat-me");
+// console.log(catMe()); // random cat get printed each time
+// run the script using $node jsfilename
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-}); // app.listen() server start krta hai, 3000 is port
-// file run kr do
-// go to https://localhost:3000
+//
+// server create krne se pahle ek command chalao
+// $npm init -y
+// this command means we have initialized a node application
+// Create server using express (install express package)
+// npm i express
+// hr ek package ko access krne ke liye hum use require krte hai, and then ek constant me store kr dete hai
+// server create krne ke liye express() use krte hai, // is se server sirf create hota hai, start nahi
+// server ko bhi ek constant app me tore kr do
+// server start krne ke liye
+// app.listen(3000); likh doge toh server chal jaega on the port 3000,
+// run the script $node index.js
+// browser me http://localhost:3000 pe jaoge to is server ko access kr paoge
+// abhi server koi output nahi de rha because we havn't programmed it to give responses
+
+const express = require("express"); // express ki functioanlities use kr skenge express const ka use krke
+const app = express(); // server created using express() and stored in app const, server created not started
+
+app.listen(3000); // server started on port 3000
+// script run kro $node index.js
+// browser me http://localhost:3000 pe jao
