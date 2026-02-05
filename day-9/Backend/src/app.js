4295b462a579e84c5a310deb2bb49a190e5de77f
anilsolanki9 <anilsolanki1192@gmail.com>
2026-02-05 20:01:22 +0530

    Frontend integration day-9 complete

--- day-9/Backend/src/app.js
+++ day-9/Backend/src/app.js
@@ -2,12 +2,18 @@
 // - create server
 // - config server
 
-const express = require('express');
+const express = require("express");
 // Importing model, so that we can perform CRUD in database
-const noteModel = require('./models/note.model');
-const cors = require('cors');
+const noteModel = require("./models/note.model");
+const cors = require("cors");
+
+// Path
+const path = require("path");
 
 const app = express();
+
+app.use(express.static("./public"));
+// ye middleware public folder me present sare assets files ko publically available kra deti hai, and now after http://localhost:3000/ ke bad jo bhi ayega woh agar public folder ke bad ke path se match hota hai, like /index.html or /assets.sshshshhshhs.css , or /assets.gstssgssshsjs.js etc toh woh file response me bhej dega, and agar nahi milegi toh obvsouly index.html file bhej dega because its a wildcard
 
 // Enables the CORS request to our server, so that now it can listen to cross-origin requests
 app.use(cors());
@@ -23,7 +29,7 @@ model ke sare operations asyncronous hote hai, so async await use krte hai.
 // input data format {title, description}
 // APIs ke sare routes (endpoints) hmesha /api/endpoint-name aise hoga
 
-app.post('/api/notes', async (req, res) => {
+app.post("/api/notes", async (req, res) => {
   // req.body se title or description destructure kiya
   // req.body ka data access krne ke liye middleware use krna pdega express.json() , write app.use(express.json());
   const { title, description } = req.body;
@@ -34,7 +40,7 @@ app.post('/api/notes', async (req, res) => {
   const note = await noteModel.create({ title, description });
 
   res.status(201).json({
-    message: 'Note created successfully',
+    message: "Note created successfully",
     note,
   });
 });
@@ -42,20 +48,20 @@ app.post('/api/notes', async (req, res) => {
 // GET API, fetch all notes data from MongoDB and send them in response
 // Get all notes
 
-app.get('/api/notes', async (req, res) => {
+app.get("/api/notes", async (req, res) => {
   // model.find() => model se bne hue collection me saved sare data ko leke aata hai, array of objects ke form me return krta hai
   const notes = await noteModel.find();
 
   // response me notes ko send kr denge
   res.status(200).json({
-    message: 'Fetched all notes successfully',
+    message: "Fetched all notes successfully",
     notes,
   });
 });
 
 // DELETE Api, id ki help se note delete krenge.
 // Delete a note, using id, which we get through req.params.id
-app.delete('/api/notes/:id', async (req, res) => {
+app.delete("/api/notes/:id", async (req, res) => {
   // req.params object me hmare dynamic parameter ki value ayegi id name se
   const id = req.params.id;
   // find by id and delete krta hai, ye _id ko dhundhta hai, and us se connected note ko delete kr deta hai.
@@ -63,24 +69,42 @@ app.delete('/api/notes/:id', async (req, res) => {
   await noteModel.findByIdAndDelete(id);
 
   res.status(200).json({
-    message: 'Note deleted successfully',
+    message: "Note deleted successfully",
   });
 });
 
 // update a note, get id by params, and data by request body
 // input data {description}
 
-app.patch('/api/notes/:id', async (req, res) => {
+app.patch("/api/notes/:id", async (req, res) => {
   const id = req.params.id;
-  const { description } = req.body;
+  const { title, description } = req.body;
   // update krta hai, pahla arg id, dusra wo data jo update krna hai.
   // element me description field me new description data update kr degi
-  
-  await noteModel.findByIdAndUpdate(id, { description });
+
+  await noteModel.findByIdAndUpdate(id, { title, description });
 
   res.status(200).json({
-    message: 'Note updates successfully',
+    message: "Note updates successfully",
   });
+});
+
+// Wild Card
+app.use("*name", (req, res) => {
+  // res.send("This is wild card");
+  // Ye wild card route pe html file bhej dega
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+  // when html get loaded it requests for js and css file, but don't get it
+  // The request for js and css look like these
+  // http://localhost:3000/assets/index-C86sZUPU.css
+  // http://localhost:3000/assets/index-BRzChCtq.js
+  // but its always returning index.html file because we havn't programmeed server to response these routes
+  // Thus we have to use one more middleware app.use(express.static('./public'))
+  // Ye public folder me present sare data assets ko publically available kr deta hai.
+  // means ab //
+  // http://localhost:3000/assets/index-C86sZUPU.css
+  // http://localhost:3000/assets/index-BRzChCtq.js
+  // can be accessed
 });
 
 // exporting app, to use in the server.js file
--- day-9/Frontend/src/App.jsx
+++ day-9/Frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 // axios install kr lo,
 // React me api call krne ke liye axios use krte hai....
 // Server(Backend) me CORS enabled hona chahiye, it is must todo
 // $npm i axios
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [editMode, setEditMode] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
 
+  // to check how many time the App() component is rendering
   // console.log('Hello'); // debugg purpose
 
   function fetchNotes() {
     // Sare notes ko laane ke liye
     // Ye request krenge tab CORS error ayega, isko mitigate krne ke liye-> backend me cors package install krke, app.js me cors require krke, is middelware ko use kr lo, app.use(cors()); jis se server CORS requests accept and respond kr paega
-    axios.get('http://localhost:3000/api/notes').then((res) => {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
       // console.log(res.data.notes);
       // database se notes ka data aa jane pe notes statevariable me value de di jaegi
       setNotes(res.data.notes);
@@ -32,27 +33,52 @@ function App() {
     // prevent page reload
     e.preventDefault();
 
-    // Form submit pe ye POST api request call hogi, and ek note save ho jaega.
-    axios
-      .post('http://localhost:3000/api/notes', {
-        title,
-        description,
-      })
-      .then((res) => {
-        console.log(res.data.note); // for debug purpose only
-        // form submit hone pe data post ho jane ke bad, fetch chalaenge taki latest all notes ka data fetch ho jaye. and then useState ki vajah se react use hr jagah change kr dega
-        fetchNotes();
-      });
+    const { title, description } = e.target.elements;
+    // console.log(title.value, description.value);
 
-    // clear the fields
-    setTitle('');
-    setDescription('');
+    if (!editMode) {
+      // Form submit pe ye POST api request call hogi, and ek note save ho jaega.
+      axios
+        .post("http://localhost:3000/api/notes", {
+          title: title.value,
+          description: description.value,
+        })
+        .then(() => {
+          // console.log(res.data.note); // for debug purpose only
+          // form submit hone pe data post ho jane ke bad, fetch chalaenge taki latest all notes ka data fetch ho jaye. and then useState ki vajah se react use hr jagah change kr dega
+          fetchNotes();
+          e.target.reset();
+        });
+    } else {
+      axios
+        .patch(`http://localhost:3000/api/notes/${currentId}`, {
+          title: title.value,
+          description: description.value,
+        })
+        .then((res) => {
+          console.log(res.data);
+          fetchNotes();
+          e.target.reset();
+          setEditMode(false);
+          setCurrentId(null);
+        });
+    }
   };
+
+  // Note pe delete btn click krne pe
+  function deleteHandler(id) {
+    axios.delete(`http://localhost:3000/api/notes/${id}`).then((res) => {
+      console.log(res.data.message);
+      fetchNotes();
+    });
+  }
+
+  // Homework Patch (modify note) vala code likho
 
   return (
     <main className="p-5">
       <form
-        className="bg-zinc-500 w-fit px-5 py-2 mb-5 rounded-lg flex gap-4 items-end"
+        className="bg-zinc-500 w-fit px-5 py-2 mb-5 rounded-lg flex gap-4 items-end relative"
         onSubmit={submitHandller}
       >
         <div>
@@ -61,15 +87,11 @@ function App() {
               Title
             </label>
             <input
+              required="true"
               name="title"
               placeholder="Enter title"
               type="text"
-              id="title"
               className="bg-zinc-600 border-white/50 border rounded-md outline-none px-3 py-1 focus:border-blue-400 transition-all duration-200 focus:scale-105 focus:shadow-md mt-1"
-              value={title}
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
             />
           </div>
           <div className="formField flex flex-col mt-2">
@@ -77,15 +99,11 @@ function App() {
               Description
             </label>
             <input
+              required="true"
               name="description"
               placeholder="Enter Description"
               type="text"
-              id="title"
               className="bg-zinc-600 border-white/50 border rounded-md outline-none px-3 py-1 focus:border-blue-400 transition-all duration-200 focus:scale-105 focus:shadow-md mt-1"
-              value={description}
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
             />
           </div>
         </div>
@@ -93,7 +111,7 @@ function App() {
           className="flex justify-center bg-white/50 font-bold text-zinc-600 px-5 py-1 rounded-lg active:scale-95 transition-all duration-200 cursor-pointer hover:scale-105"
           type="submit"
         >
-          Submit
+          {editMode ? "Update Note" : "Create Note"}
         </button>
       </form>
       <div className="notes flex gap-3 flex-wrap">
@@ -102,12 +120,31 @@ function App() {
           return (
             <div
               key={idx}
-              className="note bg-zinc-500 w-100 px-5 py-3 rounded-md shadow-lg shadow-black/60"
+              className="note relative bg-zinc-500 w-100 px-5 py-3 rounded-md shadow-lg shadow-black/60"
             >
               <h2 className="title text-2xl font-bold">{note.title}</h2>
               <p className="description bg-blue-100 text-black/80 px-3 py-1 mt-2 rounded-md font-semibold">
                 {note.description}
               </p>
+              <div className="absolute top-2 right-2 flex gap-2">
+                <button
+                  onClick={() => {
+                    setEditMode(true);
+                    setCurrentId(note._id);
+                  }}
+                  className="px-3 py-1 text-xs font-bold flex items-center justify-center rounded-sm shadow-md bg-emerald-500 hover:scale-110 active:scale-95 transition-all duration-200 cursor-pointer"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => {
+                    deleteHandler(note._id);
+                  }}
+                  className="px-3 py-1 text-xs font-bold flex items-center justify-center rounded-sm shadow-md bg-red-500 hover:scale-110 active:scale-95 transition-all duration-200 cursor-pointer"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           );
         })}
