7282421025e87e2c7f3e244045cbc58f080201cc
anilsolanki9 <anilsolanki1192@gmail.com>
2026-05-11 06:42:36 +0530

    Feed Ui working with like identification done

--- day-14/Insta_project/Frontend/src/features/post/pages/Feed.jsx
+++ day-14/Insta_project/Frontend/src/features/post/pages/Feed.jsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react";
 import Post from "../components/Post";
+import { usePost } from "../hooks/usePost";
 import "../styles/feed.scss";
 
 const Feed = () => {
+  const { loading, feed, handleGetFeed } = usePost();
+
+  useEffect(() => {
+    handleGetFeed();
+  }, []);
+
+  if (loading || !feed) {
+    return (
+      <main>
+        <h3>Feed is Loading...</h3>
+      </main>
+    );
+  }
+
   return (
     <main className="feed-page">
       <div className="feed">
         <div className="posts">
-          <Post />
-          <Post />
-          <Post />
+          {feed.map((post) => {
+            return <Post key={post._id} post={post} user={post.user} />;
+          })}
         </div>
       </div>
     </main>
---
+++ day-14/Insta_project/Frontend/src/features/post/post.context.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const PostContext = createContext();
+
+export const PostContextProvider = ({ children }) => {
+  const [feed, setFeed] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [post, setPost] = useState(null);
+
+  return <PostContext.Provider value={{ loading, setLoading, post, setPost, feed, setFeed }}>{children}</PostContext.Provider>;
+};
